c3582e2ed95cec042599bfa8c1c68b82b78a0f5c
Rodrigo Motta <rmotta.net@gmail.com>
2025-12-21 17:08:00 -0300

    feat(github): add public repository enumeration support

--- lib/github_api.sh
+++ lib/github_api.sh
@@ -20,3 +20,29 @@ parse_github_user() {
         following
     }'
 }
+
+
+# --- Repository Functions ---
+# SRP: Responsabilidade única de buscar os repositórios brutos
+get_github_repos_raw() {
+    local username=$1
+    # O endpoint padrão para listar repositórios de um usuário
+    # per_page=100 é o máximo permitido por página no GitHub
+    curl -sf -A "UnauthScout-Scanner" \
+        "${GITHUB_BASE_URL}/users/${username}/repos?type=public&per_page=100&sort=updated"
+}
+
+# SRP: Responsabilidade única de filtrar e formatar a saída para o Scout
+parse_github_repos() {
+    # Mapeamos os campos do GitHub para manter um padrão próximo ao do GitLab
+    jq '.[] | {
+        id,
+        name: .name,
+        full_name: .full_name,
+        description,
+        url: .html_url,
+        stars: .stargazers_count,
+        forks: .forks_count,
+        language
+    }'
+}
